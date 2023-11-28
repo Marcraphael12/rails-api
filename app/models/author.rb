41ce77cdc39bf31d2372657758d98d586b6b672e
Marc raphael <marcraphael979@gmail.com>
2023-11-28 14:33:11 +0100

    update db with models validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+  validates :name, presence: true, uniqness: true
+  validates :age, presence: true
+
+  has_may :books, dependent: :destroy
 end
--- db/migrate/20231128114344_create_authors.rb
+++ db/migrate/20231128114344_create_authors.rb
@@ -2,8 +2,7 @@ class CreateAuthors < ActiveRecord::Migration[7.1]
   def change
     create_table :authors do |t|
       t.string :name
-      t.string :age
-      t.integer :
+      t.integer :age
 
       t.timestamps
     end
